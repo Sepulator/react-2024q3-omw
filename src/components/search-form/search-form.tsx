d12ef37aba72d6060601824cdb66393f42d853fa
Yuri Skrypal <yjura@protonmail.com>
2024-07-05 20:29:09 +0300

    feat: add local storage for query

--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, Component } from 'react';
+import { ChangeEvent, Component, createRef, RefObject } from 'react';
 import './search-form.css';
 
 type Props = {
@@ -6,16 +6,47 @@ type Props = {
 };
 
 export class SearchForm extends Component<Props> {
+  input: RefObject<HTMLInputElement>;
+
+  constructor(props: Props) {
+    super(props);
+    this.input = createRef<HTMLInputElement>();
+  }
+
   onSubmit = (e: ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
     const query = e.target.text.value.toLowerCase().trim();
     this.props.onSearchChange(query);
   };
 
+  getLocalStorage = () =>
+    (this.input.current!.value = localStorage.getItem('query-text') || '');
+
+  setLocalStorage = () =>
+    localStorage.setItem('query-text', this.input.current!.value);
+
+  componentDidMount() {
+    this.getLocalStorage();
+    window.addEventListener('beforeunload', this.setLocalStorage);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.setLocalStorage);
+  }
+
+  onInput = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: e.target.value });
+  };
+
   render() {
     return (
       <form className="search-form" onSubmit={this.onSubmit}>
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          placeholder="Type name from Rick and Morty"
+          ref={this.input}
+        />
         <button className="btn">Search</button>
       </form>
     );
