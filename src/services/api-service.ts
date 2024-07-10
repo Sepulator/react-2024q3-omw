939b20304efdc6b76187bbd62a38542852507979
Yuri Skrypal <yjura@protonmail.com>
2024-07-10 13:59:03 +0300

    fix: remove comment

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -15,11 +15,6 @@ export class ApiService {
 
   async getRes<T>(url: string): Promise<T> {
     const res = await fetch(`${this.apiBase}${url}`);
-
-    // if (!res.ok) {
-    //   throw new Error(`Could not fetch ${url}, status ${res.status}`);
-    // }
-
     return (await res.json()) as T;
   }
 
