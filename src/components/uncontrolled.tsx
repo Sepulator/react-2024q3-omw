d7f851f7cb9cfb6e269a9ba6b640a5e11b41d56b
Yuri Skrypal <yjura@protonmail.com>
2024-08-16 19:41:14 +0300

    feat: add yup validation & errors display

--- src/components/uncontrolled.tsx
+++ src/components/uncontrolled.tsx
@@ -1,7 +1,32 @@
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import * as Yup from 'yup';
 
 import styles from '@styles/form.module.css';
 import global from '@styles/global.module.css';
+
+export interface FormValues {
+  name: string;
+  age: string;
+  email: string;
+  password: string;
+  repeat: string;
+  country: string;
+  gender: string;
+  upload: File;
+  accept: boolean;
+}
+
+interface FormErrors {
+  name?: string;
+  age?: string;
+  email?: string;
+  password?: string;
+  repeat?: string;
+  country?: string;
+  gender?: string;
+  upload?: string;
+  accept?: boolean;
+}
 
 const d = [
   'Type name',
@@ -12,67 +37,200 @@ const d = [
   'Start typing county',
 ];
 
+const fileTypes = ['image/jpeg', 'image/png'];
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .matches(/^[A-Z]/, 'Name must start with an uppercase letter')
+    .required()
+    .label('Name'),
+  age: Yup.number()
+    .transform((value: string) => (Number.isNaN(value) ? null : value))
+    .positive()
+    .integer()
+    .required('Age must be a number')
+    .label('Age'),
+  email: Yup.string().email().required().label('Email'),
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters long')
+    .matches(/[A-Z]/, 'Password contain at least one uppercase letter')
+    .matches(/[a-z]/, 'Password contain at least one lowercase letter')
+    .matches(/[0-9]/, 'Password must contain at least one number')
+    .matches(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      'Password contain at least one special character'
+    )
+    .required()
+    .label('Password'),
+  repeat: Yup.string()
+    .oneOf([Yup.ref('password')], 'Password must match')
+    .required()
+    .label('Password'),
+  country: Yup.string().required().label('Country'),
+  gender: Yup.string().required().label('Gender'),
+  upload: Yup.mixed<File>()
+    .test('fileSize', 'File size must be less than 1MB', (value) => {
+      return value && value.size <= 1024 * 1024;
+    })
+    .test('fileType', 'File extension must be .jpg, .jpeg, .png', (value) => {
+      return value && fileTypes.includes(value.type);
+    })
+    .required()
+    .label('Image File'),
+  accept: Yup.string()
+    .required('Please accept Terms and Conditions')
+    .label('Terms'),
+});
+
 export default function Uncontrolled() {
-  function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  const [errors, setErrors] = useState<FormErrors>({});
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
     const formData = new FormData(event.currentTarget);
     const formObject = Object.fromEntries(formData.entries());
-    console.log(formObject);
+
+    try {
+      await schema.validate(formObject, { abortEarly: false });
+      setErrors({});
+    } catch (validationErrors) {
+      const newErrors: Record<string, string> = {};
+
+      if (validationErrors instanceof Yup.ValidationError) {
+        validationErrors.inner.forEach((error) => {
+          if (error.path) {
+            newErrors[error.path] = error.message;
+          }
+        });
+      }
+      setErrors(newErrors);
+    }
   }
 
   return (
     <div className={styles.wrap}>
-      <form className={styles.form} onSubmit={onSubmit}>
-        <label htmlFor="text" className={styles.label}>
-          Name
-          <input id="text" type="text" name="text" placeholder={d[0]} />
+      <form
+        className={styles.form}
+        onSubmit={(event) => {
+          event.preventDefault();
+          void onSubmit(event);
+        }}
+      >
+        <label
+          htmlFor="name"
+          className={`${styles.label} ${errors.name ? styles.error : ''}`}
+        >
+          {errors.name ? errors.name : 'Name'}
+          <input id="name" type="text" name="name" placeholder={d[0]} />
         </label>
-        <label htmlFor="number" className={styles.label}>
-          Age
-          <input id="number" type="number" name="number" placeholder={d[1]} />
+        <label
+          htmlFor="age"
+          className={`${styles.label} ${errors.age ? styles.error : ''}`}
+        >
+          {errors.age ? errors.age : 'Age'}
+          <input id="age" type="text" name="age" placeholder={d[1]} />
         </label>
-        <label htmlFor="email" className={styles.label}>
-          Email
-          <input id="email" type="email" name="email" placeholder={d[2]} />
+        <label
+          htmlFor="email"
+          className={`${styles.label} ${errors.email ? styles.error : ''}`}
+        >
+          {errors.email ? errors.email : 'Email'}
+          <input id="email" type="text" name="email" placeholder={d[2]} />
         </label>
-        <label htmlFor="pass" className={styles.label}>
-          Password
-          <input id="pass" type="password" name="pass" placeholder={d[3]} />
+        <label
+          htmlFor="password"
+          className={`${styles.label} ${errors.password ? styles.error : ''}`}
+        >
+          {errors.password ? errors.password : 'Password'}
+          <input
+            id="password"
+            type="password"
+            name="password"
+            placeholder={d[3]}
+          />
         </label>
-        <label htmlFor="repeat" className={styles.label}>
-          Password
+        <label
+          htmlFor="repeat"
+          className={`${styles.label} ${errors.repeat ? styles.error : ''}`}
+        >
+          {errors.repeat ? errors.repeat : 'Password'}
           <input id="repeat" type="password" name="repeat" placeholder={d[4]} />
         </label>
-        <label htmlFor="file" className={styles.labelFile}>
-          {''}
+        <label
+          htmlFor="country"
+          className={`${styles.label} ${errors.country ? styles.error : ''}`}
+        >
+          {errors.country ? errors.country : 'Country'}
           <input
-            id="file"
-            type="file"
-            name="file"
-            accept=".png, .jpg, .jpeg"
-            className={styles.file}
-          />
-        </label>
-        <label htmlFor="city" className={styles.label}>
-          Select country
-          <input
-            id="city"
+            id="country"
             type="search"
-            name="city"
+            name="country"
             placeholder={d[5]}
-            list="cities"
+            list="countries"
             size={20}
           />
-          <datalist id="cities">
+          <datalist id="countries">
             <option value="Russia" />
             <option value="USA" />
             <option value="Ukraine" />
             <option value="Uzbekistan" />
           </datalist>
         </label>
-        <label htmlFor="checkbox" className={styles.labelCheck}>
-          <input id="checkbox" type="checkbox" name="checkbox" />
-          Accept Terms and Conditions
+        <div>
+          <span
+            className={`${styles.label} ${errors.gender ? styles.error : ''}`}
+          >
+            {errors.gender ? errors.gender : 'Gender'}
+          </span>
+          <div className={styles.radioWrap}>
+            <label
+              htmlFor="male"
+              className={`${styles.labelCheck} ${errors.gender ? styles.error : ''}`}
+            >
+              <input id="male" type="radio" name="gender" value="male" />
+              Male
+            </label>
+            <label
+              htmlFor="female"
+              className={`${styles.labelCheck} ${errors.gender ? styles.error : ''}`}
+            >
+              <input id="female" type="radio" name="gender" value="female" />
+              Female
+            </label>
+            <label
+              htmlFor="none"
+              className={`${styles.labelCheck} ${errors.gender ? styles.error : ''}`}
+            >
+              <input id="none" type="radio" name="gender" value="none" />
+              None
+            </label>
+          </div>
+        </div>
+        <div className={styles.fileWrap}>
+          <span
+            className={`${styles.label} ${errors.upload ? styles.error : ''}`}
+          >
+            {errors.upload ? errors.upload : 'Image file'}
+          </span>
+          <label
+            htmlFor="upload"
+            className={`${styles.labelFiles} ${errors.upload ? styles.error : ''}`}
+          >
+            {''}
+            <input
+              id="upload"
+              type="file"
+              name="upload"
+              accept=".png, .jpg, .jpeg"
+              className={styles.file}
+            />
+          </label>
+        </div>
+        <label
+          htmlFor="accept"
+          className={`${styles.labelCheck} ${errors.accept ? styles.error : ''}`}
+        >
+          <input id="accept" type="checkbox" name="accept" />
+          {errors.accept ? errors.accept : 'Accept Terms and Conditions'}
         </label>
         <div className={styles.btnWrap}>
           <button aria-label="reset" className={global.btnRound} type="reset">
